ca113cff3f0b9a292df8e1dbcbf612b87045ff9e
Srikar <srikarsamanchi1999@gmail.com>
2021-06-20 16:28:49 +0530

    Added : Binary search tree Uc2

---
+++ MyBinaryTreeTest2.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.binarysearchtree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryTreeTest2 {
+	@Test
+	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree()
+	{
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+      		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(3,size);
+	}
+}
