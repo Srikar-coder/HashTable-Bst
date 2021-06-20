b7b747e26af1144633eb4c3a194b4787e7c009f9
Srikar <srikarsamanchi1999@gmail.com>
2021-06-20 16:08:50 +0530

    Added : hash table Uc2

---
+++ MyLinkedHashMapTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.hashtable;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class MyLinkedHashMapTest {
+
+	@Test
+	public void givenASentence_WhenWordsAreAddedToListShould_ReturnParanoidFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but" + 
+	                      "because they keep putting themselves deliberately into" + 
+				          "paranoid avoidable situations";
+        MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String[] words = sentence.toLowerCase().split(" ");
+		for(String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if(value == null) value = 1;
+			else value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		int frequency = myLinkedHashMap.get("paranoid");
+		System.out.println(myLinkedHashMap);
+		Assert.assertEquals(3, frequency);
+	}
+}
